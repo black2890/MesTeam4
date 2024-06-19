e722700bb639c3d5585efcb0ff285d7fb98ad3a7
black2890 <100751771+black2890@users.noreply.github.com>
2024-06-19 11:50:03 +0900

    add side bar

--- src/main/java/com/mesproject/repository/VendorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mesproject.repository;
-
-import com.mesproject.entity.Vendor;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VendorRepository extends JpaRepository<Vendor, Long> {
-}
